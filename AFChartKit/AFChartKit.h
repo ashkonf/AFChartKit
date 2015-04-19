1b0fee6d86592d71592d1c7ebf7faf52a91febf5
Ashkon Farhangi <ashkonfarhangi@Ashkons-MacBook-Pro.local>
2015-04-19 08:44:30 -0700

    renamed

---
+++ AFChartKit/AFChartKit.h
@@ -0,0 +1,10 @@
+//
+//  AFChartKit.h
+//  AFChartKit
+//
+//  Created by Ashkon Farhangi on 4/19/15.
+//  Copyright (c) 2015 Ashkon Farhangi. All rights reserved.
+//
+
+#import "AFLineChart.h"
+#import "AFLineChartDefinitions.h"
